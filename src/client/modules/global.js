a58d84ebc50eaf258a8313b045024fc8985b9d8d
Dev Myna <var.devmyna@gmail.com>
2022-10-08 18:07:04 -0300

    Correção do Diretório Public/Src

---
+++ src/client/modules/global.js
@@ -0,0 +1,5 @@
+const main=()=>{const obj={// Variáveis
+__rootdir:"/media/veracrypt1/prog/reps/my/Central/",// Funções
+root:dir=>{// Definir Diretório Root
+return ReactDOM.createRoot(document.querySelector(dir))}};return{obj}};export const global=main().obj;
+//# sourceMappingURL=global.js.map
