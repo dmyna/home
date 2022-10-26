254a6576f8fc7328eda4663dcd5579fc764d110d
Dev Myna <var.devmyna@gmail.com>
2022-10-26 11:54:06 -0300

    Adição do Layout na Página Principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react'
-
+import * as ReactDomServer from 'react-dom/server'
+import ReactHtmlParser from 'react-html-parser';
 import Layout from '../components/layout'
-
 import style from '../style/css.module.scss'
 
-const main = () => {
+export const getServerSideProps = () => {
+    const render = require('../modules/render')
+
+    const NavAsdButtons = ReactDomServer.renderToString(render.navegation());
+
+    return { props: { NavAsdButtons } }
+}
+
+const main = ({ NavAsdButtons }: any) => {
     return (
-        <Layout>
+        <Layout NavAsdButtons={NavAsdButtons}>
 
         </Layout>
     )
