438c92d5bfe70b0fac14eae7865afc6e9fdee539
Dev Myna <var.devmyna@gmail.com>
2022-10-26 08:52:27 -0300

    Alterações no Sistema de Compilação

--- src/client/main.js
+++
@@ -1,3 +0,0 @@
-"use strict";import{events}from"./modules/events.js";import{render}from"./modules/render.js";import{button}from"./modules/button.js";$(()=>{// Main
-events;button;render.navegation();render.mainPagePlaylists()});
-//# sourceMappingURL=main.js.map
