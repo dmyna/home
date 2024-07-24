4b5b4036d2cb105c2d1dd410377eac10a838c3ea
Dev Myna <var.devmyna@gmail.com>
2024-07-24 13:37:14 -0300

    refactor: remove AnyObj to enforce strong typing

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,29 +1,36 @@
-type AnyFunction = (...args: any[]) => any
-type AnyObj = Record<string, any>
-type UnkObj = Record<string, unknown>
-type AnyErr = {
+declare type UnknownFun = (...args: unknown[]) => unknown
+declare type UnknownObj = Record<string, unknown>
+declare type AnyErr = {
     message: string
 }
-type FactoryObj<T> = {
-    [key: string]: T[keyof T] extends AnyFunction
+
+declare type FunctionsObj<T> = {
+    [K in keyof T]: T[K] extends UnknownFun
+        ? T[K]
+        : T[K] extends object
+        ? FunctionsObj<T[K]>
+        : never;
+};
+declare type FactoryObj<T> = {
+    [key: string]: T[keyof T] extends UnknownFun
     ? T[keyof T]
     : T[keyof T] extends object
     ? FunctionsObj<T[keyof T]>
     : never;
 } & {
-    [K in keyof T]: T[K] extends AnyFunction
+    [K in keyof T]: T[K] extends UnknownFun
     ? T[K]
     : T[K] extends object
     ? FunctionsObj<T[K]>
     : never;
 };
 
-type Factory<T> = () => FactoryObj<T>;
+declare type Factory<T> = () => FactoryObj<T>;
 
-type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
+declare type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
 
-interface JSONObject {
+declare interface JSONObject {
     [x: string]: JSONValue;
 }
 
-interface JSONArray extends Array<JSONValue> {}
+declare interface JSONArray extends Array<JSONValue> {}
