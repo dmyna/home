001824b8f762e55088735d052ba2fd3694d8e5b0
Dev Myna <var.devmyna@gmail.com>
2024-07-27 14:54:56 -0300

    chore: configure tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 /** @format */
 
-import "../style/themes/dark-theme/geral.scss";
-import "../style/global.scss";
+import "-/style/themes/dark-theme/geral.scss";
+import "-/style/global.scss";
+import "-/tailwind.css";
 
 import { JSX } from "react";
 import { AppProps } from "next/app";
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @format */
+
+import type { Config } from "tailwindcss";
+
+export default {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    darkMode: "selector",
+} satisfies Config;
