5ba60d0a837acf1502747493881f9cfee7f96d8b
Dev Myna <var.devmyna@gmail.com>
2024-07-28 17:23:46 -0300

    feat: implement Tailwind  and remove sass for `PresentationDiv` component

--- src/components/PresentationDiv.tsx
+++ src/components/PresentationDiv.tsx
@@ -4,9 +4,6 @@
 import React from "react";
 import { JSX } from "react";
 
-// * Style
-import style from "S/components/presentation_div.module.scss";
-
 // * Text
 import PresentationText from "M/PresentationText.mdx";
 
@@ -18,8 +15,14 @@ class PresentationDiv extends React.Component<React.HTMLProps<HTMLDivElement>> {
 
     render(): JSX.Element {
         return (
-            <div className={style.presentationDiv}>
-                <div id={style.presentationText}>
+            <div className='flex justify-center align-middle fle-col w-full py-0 px-[10vw]'>
+                <div
+                    className={
+                        "font-display font-semibold " +
+                        "text-justify md:text-center text-sm md:text-xl " +
+                        "text-gray-200 drop-shadow-[0_0_3px_rgba(0,0,0,0.5)]"
+                    }
+                >
                     <PresentationText />
                 </div>
             </div>
