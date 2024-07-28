74c40edb6f9a5e2c47ef871670091422df7282a9
Dev Myna <var.devmyna@gmail.com>
2024-07-28 17:21:47 -0300

    feat: implement Tailwind  and remove sass for `Layout` component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,81 +5,65 @@ import { JSX } from "react";
 import Head from "next/head";
 
 // * Internal Modules
-import NavAsdButtons from "./NavAsdButtons";
+import NavAsdButtons, { vwWidth } from "./NavAsdButtons";
 
 // * Typing
 import utilsTypes from "../utils/types";
 export namespace types {
     export type receive = {
         fullview: boolean;
-        navAsdData:  utilsTypes.NavAsdData;
+        navAsdData: utilsTypes.NavAsdData;
         children: JSX.Element;
     };
 }
-// * Style
-import style from "../style/components/layout.normal.module.scss";
-import fullStyle from "../style/components/layout.full.module.scss";
-
 // * Main
-const Layout = ({ fullview, navAsdData, children }: types.receive): JSX.Element => {
+const Layout = ({
+    fullview,
+    navAsdData,
+    children,
+}: types.receive): JSX.Element => {
+    const head = (
+        <Head>
+            <title>Dev Myna</title>
+            <meta name='author' content='Dev Myna' />
+            <link
+                rel='shortcut icon'
+                href='https://avatars.githubusercontent.com/u/72279988'
+                type='image/x-icon'
+            />
+        </Head>
+    );
+
     const element = {
         fullview: (
-            <div className={"body " + fullStyle.body}>
-                <Head>
-                    <title>Dev Myna</title>
-                    <meta name='author' content='Dev Myna' />
-                    <link
-                        rel='shortcut icon'
-                        href='https://avatars.githubusercontent.com/u/72279988'
-                        type='image/x-icon'
-                    />
-                </Head>
-                <div className={"mainFlex " + fullStyle.mainFlex}>
-                    <main className={fullStyle.main}>
-                        <article className={"mainContent " + fullStyle.main}>
-                            {children}
-                        </article>
+            <div className='w-dvw h-dvh'>
+                {head}
+                <div className='w-dvw h-dvh'>
+                    <main className='w-full'>
+                        <article>{children}</article>
                     </main>
                 </div>
-                <div
-                    className={
-                        "floatBoxesController " + fullStyle.floatBoxesController
-                    }
-                ></div>
+                <div></div>
             </div>
         ),
         normal: (
-            <div className={"body " + style.body}>
-                <Head>
-                    <title>Dev Myna</title>
-                    <meta name='author' content='Dev Myna' />
-                    <link
-                        rel='shortcut icon'
-                        href='https://avatars.githubusercontent.com/u/72279988'
-                        type='image/x-icon'
-                    />
-                </Head>
-                <aside className={"navegation " + style.navegation}>
+            <div className='flex overflow-hidden h-dvh'>
+                {head}
+                <aside className='w-max flex-shrink-0 bg-gray-950'>
                     <NavAsdButtons uiData={navAsdData} />
                 </aside>
-                <div className={"mainFlex " + style.mainFlex}>
-                    <header className={"mainHeader " + style.mainHeader}>
-                        <div></div>
+                <div className='flex flex-col h-dvh w-full bg-gray-950'>
+                    <header
+                        className={`h-[${vwWidth}] w-full font-bold font-display text-xl`}
+                    >
+                        <div className='flex justify-center align-middle w-full h-full'></div>
                     </header>
-                    <main className={"mainContent"}>
-                        <article className={style.mainContent}>
-                            {children}
-                        </article>
+                    <main className='flex-grow overflow-y-auto rounded-ss-lg bg-gray-900'>
+                        <article className='py-8 px-32'>{children}</article>
                     </main>
-                    <footer
-                        className={"mainFooter " + style.mainFooter}
-                    ></footer>
+                    <footer></footer>
                 </div>
-                <div
-                    className={
-                        "floatBoxesController " + style.floatBoxesController
-                    }
-                ></div>
+                <div></div>
             </div>
         ),
     };
